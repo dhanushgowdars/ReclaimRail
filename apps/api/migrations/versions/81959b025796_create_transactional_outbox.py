ffe49b9275fb01ee241db72181b8744749329ee8
dhanushrs <dhanushrs981@gamil.com>
2026-08-24 13:40:17 +0530

    feat(webhooks): create outbox job atomically

--- apps/api/migrations/versions/81959b025796_create_transactional_outbox.py
+++ apps/api/migrations/versions/81959b025796_create_transactional_outbox.py
@@ -6,17 +6,17 @@ Create Date: 2026-08-24 13:25:49.221407
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "81959b025796"
-down_revision: Union[str, Sequence[str], None] = "53b58928fdb7"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = "53b58928fdb7"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
